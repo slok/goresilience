67e048738f6774560bd30e535ab26e12a4a43c45
Xabier Larrakoetxea <slok69@gmail.com>
2018-12-22 11:36:17 +0100

    Add concurrencylimit runner
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ concurrencylimit/concurrencylimit_test.go
@@ -0,0 +1,67 @@
+package concurrencylimit_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/slok/goresilience/concurrencylimit"
+	"github.com/slok/goresilience/concurrencylimit/limit"
+	mexecute "github.com/slok/goresilience/internal/mocks/concurrencylimit/execute"
+	mlimit "github.com/slok/goresilience/internal/mocks/concurrencylimit/limit"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var fOK = func(_ context.Context) error { return nil }
+var successPolicy = func(ctx context.Context, err error) limit.Result { return limit.ResultSuccess }
+
+func TestConcurrencyLimit(t *testing.T) {
+	tests := []struct {
+		name   string
+		policy concurrencylimit.ExecutionResultPolicy
+		mock   func(ml *mlimit.Limiter, me *mexecute.Executor)
+		expErr error
+	}{
+		{
+			name:   "Executing a Func should  measure the samples with the policy result and set the worker quantity based on the limiter limit.",
+			policy: successPolicy,
+			mock: func(ml *mlimit.Limiter, me *mexecute.Executor) {
+				measuredLimit := 97
+
+				// Return ok on execution.
+				me.On("Execute", mock.Anything).Once().Return(nil)
+
+				// Expect measuring the sample after the execution.
+				ml.On("MeasureSample", mock.Anything, 0, limit.ResultSuccess).Once().Return(measuredLimit)
+
+				// Expect setting the limit after measuring with the limiter.
+				me.On("SetWorkerQuantity", measuredLimit)
+
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			// Mocks.
+			ml := &mlimit.Limiter{}
+			me := &mexecute.Executor{}
+			// First limit get/set on creation (prepare).
+			ml.On("GetLimit").Once().Return(0)
+			me.On("SetWorkerQuantity", 0).Once()
+			test.mock(ml, me)
+
+			cfg := concurrencylimit.Config{
+				Limiter:  ml,
+				Executor: me,
+			}
+
+			runner := concurrencylimit.New(cfg)
+			err := runner.Run(context.TODO(), fOK)
+
+			assert.Equal(test.expErr, err)
+		})
+	}
+}
---
+++ concurrencylimit/doc.go
@@ -0,0 +1,34 @@
+package concurrencylimit
+
+// concurrencylimit it's inspired by the implementation of Netflix/concurrency-limits
+// it tries to create a concurrency controlled execution flow using adaptive algorithms
+// from TCP congestion control algorithms.
+//
+// The implementation of concurrencylimit runner is based in 3 componentes:
+//
+// - The Limiter algorithm: This is the algorithm used to calculate
+// 	 the limit of concunrrency. There are multiple to select based on the type
+// 	 of execution flow used on the Runner (client side, server side,
+//   lot's of dependencies...).
+//
+// - The Executor: The executor is the one that knows how to execute Funcs, it
+// 	 controls the concurrency, increases and decreases the number of executor
+//   workers. There are different kinds like blockers, partitioned...
+//	 One important thing is that the executors sometimes (depends on the implementation)
+//   will return a execution rejected exception (errors.ErrLimiterExecutionRejected),
+//   this doesn't count for the algorithm and could be checked by the user to do another
+//	 thing, fallback, return error or whatever.
+//
+// - The Runner: This is the one that implements goresilience.Runner, it's the
+//	 one that will be used directly by a goreslience library user. It glues
+//	 together the algorithm and the executor, knows how to handle the  user Func
+//   to be executed by the executor, knows how to measure the result to give it
+//   to the algorithm to create a new limit dynamically, sets those limits on the
+//	 executor...
+//
+// The concurrencylimit Runner has a configuration field that is the error policy
+// this policy is the one responsible to tell if the received error by the function
+// is a real error for the limit algorithm or not. By default every
+// error is treated as a failure and will count on the algorithm as an failure.
+// Another policy could be only the errors that are not of X type and the field Y
+// from the context has Z attribute to true.
